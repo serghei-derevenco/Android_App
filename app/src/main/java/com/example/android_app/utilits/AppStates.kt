f497c123ebdc334b8048a976a0f76816a274762d
Serghei Derevenco <serghei.derevenco@gmail.com>
2021-04-05 19:45:08 +0300

    add app states

---
+++ app/src/main/java/com/example/android_app/utilits/AppStates.kt
@@ -0,0 +1,16 @@
+package com.example.android_app.utilits
+
+enum class AppStates(val state: String) {
+    ONLINE("online"),
+    OFFLINE("last seen recently"),
+    TYPING("typing...");
+
+    companion object {
+        fun updateState(appStates: AppStates) {
+            REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID).child(CHILD_STATE)
+                .setValue(appStates.state)
+                .addOnSuccessListener { USER.state = appStates.state }
+                .addOnFailureListener{ showToast(it.message.toString()) }
+        }
+    }
+}
